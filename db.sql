6ffee68d3bdaf875af0a16ba0d6031ff27eca85b
Bui_Duy_Nguyen <150675541+buiduynguyenn@users.noreply.github.com>
2024-11-30 09:40:48 +0700

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,27 +1,47 @@
--- MySQL dump 10.13  Distrib 8.0.38, for Win64 (x86_64)
---
--- Host: 127.0.0.1    Database: tour_booking
--- ------------------------------------------------------
--- Server version	8.0.39
+CREATE DATABASE IF NOT EXISTS `tour_booking` /*!40100 DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_unicode_ci */;
+USE `tour_booking`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+-- Table structure for table `users`
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE `users` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `password` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
+  `email` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `full_name` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `role` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
---
+-- Dumping data for table `users`
+INSERT INTO `users` VALUES 
+(1,'user1','password123','user1@example.com','Nguyen Van A','USER','2024-11-29 23:19:31'),
+(2,'admin','admin123','admin@example.com','Admin User','ADMIN','2024-11-29 23:19:31');
+
+-- Table structure for table `tours`
+DROP TABLE IF EXISTS `tours`;
+CREATE TABLE `tours` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `price` double NOT NULL,
+  `duration` int DEFAULT NULL,
+  `location` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
+-- Dumping data for table `tours`
+INSERT INTO `tours` VALUES 
+(1,'Tour Đà Nẵng','Khám phá vẻ đẹp của Đà Nẵng trong 3 ngày.',1500000,3,'Đà Nẵng','2024-11-29 23:19:37'),
+(2,'Tour Hà Nội','Trải nghiệm văn hóa Hà Nội trong 2 ngày.',1200000,2,'Hà Nội','2024-11-29 23:19:37'),
+(3,'Tour Phú Quốc','Thư giãn tại bãi biển Phú Quốc trong 4 ngày.',2500000,4,'Phú Quốc','2024-11-29 23:19:37');
+
 -- Table structure for table `bookings`
---
-
 DROP TABLE IF EXISTS `bookings`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `bookings` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
@@ -35,25 +55,14 @@ CREATE TABLE `bookings` (
   CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
   CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
 -- Dumping data for table `bookings`
---
+INSERT INTO `bookings` VALUES 
+(1,1,1,'2024-11-29 23:19:42',2,3000000),
+(2,2,2,'2024-11-29 23:19:42',1,1200000);
 
-LOCK TABLES `bookings` WRITE;
-/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
-INSERT INTO `bookings` VALUES (1,1,1,'2024-11-29 23:19:42',2,3000000),(2,2,2,'2024-11-29 23:19:42',1,1200000);
-/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
 -- Table structure for table `reviews`
---
-
 DROP TABLE IF EXISTS `reviews`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reviews` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint NOT NULL,
@@ -65,88 +74,10 @@ CREATE TABLE `reviews` (
   KEY `user_id` (`user_id`),
   KEY `tour_id` (`tour_id`),
   CONSTRAINT `reviews_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`),
-  CONSTRAINT `reviews_chk_1` CHECK ((`rating` between 1 and 5))
+  CONSTRAINT `reviews_ibfk_2` FOREIGN KEY (`tour_id`) REFERENCES `tours` (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
---
 -- Dumping data for table `reviews`
---
-
-LOCK TABLES `reviews` WRITE;
-/*!40000 ALTER TABLE `reviews` DISABLE KEYS */;
-INSERT INTO `reviews` VALUES (1,1,1,5,'Tour rất tuyệt vời!','2024-11-29 23:19:47'),(2,2,2,4,'Dịch vụ tốt nhưng có một số vấn đề nhỏ.','2024-11-29 23:19:47');
-/*!40000 ALTER TABLE `reviews` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `tours`
---
-
-DROP TABLE IF EXISTS `tours`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tours` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `description` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `price` double NOT NULL,
-  `duration` int DEFAULT NULL,
-  `location` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tours`
---
-
-LOCK TABLES `tours` WRITE;
-/*!40000 ALTER TABLE `tours` DISABLE KEYS */;
-INSERT INTO `tours` VALUES (1,'Tour Đà Nẵng','Khám phá vẻ đẹp của Đà Nẵng trong 3 ngày.',1500000,3,'Đà Nẵng','2024-11-29 23:19:37'),(2,'Tour Hà Nội','Trải nghiệm văn hóa Hà Nội trong 2 ngày.',1200000,2,'Hà Nội','2024-11-29 23:19:37'),(3,'Tour Phú Quốc','Thư giãn tại bãi biển Phú Quốc trong 4 ngày.',2500000,4,'Phú Quốc','2024-11-29 23:19:37');
-/*!40000 ALTER TABLE `tours` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` bigint NOT NULL AUTO_INCREMENT,
-  `username` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb3_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `full_name` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `role` varchar(255) COLLATE utf8mb3_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `users`
---
-
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-INSERT INTO `users` VALUES (1,'user1','password123','user1@example.com','Nguyen Van A','USER','2024-11-29 23:19:31'),(2,'admin','admin123','admin@example.com','Admin User','ADMIN','2024-11-29 23:19:31');
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-11-30  7:25:05
+INSERT INTO `reviews` VALUES 
+(1,1,1,5,'Tour rất tuyệt vời!','2024-11-29 23:19:47'),
+(2,2,2,4,'Dịch vụ tốt nhưng có một số vấn đề nhỏ.','2024-11-29 23:19:47');
